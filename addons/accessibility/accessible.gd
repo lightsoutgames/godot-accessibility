319c1297c4b05705e25ffeb89d1e8dccfd056fe0
Nolan Darilek <nolan@thewordnerd.info>
2018-06-07 21:18:36 +0000

    Add nodes to an accessible group after they are augmented.

--- addons/accessibility/accessible.gd
+++ addons/accessibility/accessible.gd
@@ -12,6 +12,9 @@ func gui_input():
     print("GUI input.")
 
 func _init(node):
+    if node.is_in_group("accessible"):
+        return
+    node.add_to_group("accessible")
     # print(node.get_path())
     self.node = node
     self.node.connect("focus_entered", self, "focused")
