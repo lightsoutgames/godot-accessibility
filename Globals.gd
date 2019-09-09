d42d913f296d9a58d7458cbdc0fe6fd8224ab32e
Nolan Darilek <nolan@thewordnerd.info>
2019-09-09 14:53:44 -0500

    Remove sample game and refactor plugin for use in separate games. The standalone game will probably be refactored to an accessible starter game.

--- Globals.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-const TTS = preload("res://addons/godot-tts/godot_tts.gdns")
-var tts
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    tts = TTS.new()
