e82fd7f8b8f80a346eda93caae015fd84876b935
Nolan Darilek <nolan@thewordnerd.info>
2019-09-24 09:20:45 -0500

    Add `ScreenReader` as a custom node in the editor plugin.

--- Plugin.gd
+++ Plugin.gd
@@ -10,3 +10,7 @@ func _enter_tree():
     get_tree().root.call_deferred("add_child", screen_reader)
     call_deferred("connect", "scene_changed", screen_reader, "set_initial_scene_focus")
     call_deferred("connect", "main_screen_changed", screen_reader, "set_initial_screen_focus")
+    add_custom_type("ScreenReader", "Node", preload("ScreenReader.gd"), preload("icon.png"))
+
+func _exit_tree():
+    remove_custom_type("ScreenReader")
--- ScreenReader.gd
+++ ScreenReader.gd
@@ -1,3 +1,4 @@
+tool
 extends Node
 
 var Accessible = preload("Accessible.gd")
